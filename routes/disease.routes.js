04ee784518a57a37c7860ba7667ed5f58a2aac21
danmesfin <danielmsfn@gmail.com>
2023-05-13 12:06:04 +0300

    updated disease routes

--- routes/disease.routes.js
+++ routes/disease.routes.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const DiseaseController = require("../controllers/diseases.controllers");
-//const { verifyToken } = require("../middleware/auth.middleware");
 
-router.get("/control-methods/:disease", DiseaseController.getControlMethods);
+const {
+  getAllDiseaseControlMethods,
+  getDiseaseControlMethodsByDiseaseName,
+  createDiseaseControlMethod,
+  updateDiseaseControlMethodByName,
+  deleteDiseaseControlMethodByName,
+} = require("../controllers/disease.controller");
+
+// Get all disease control methods
+router.get("/disease-control", getAllDiseaseControlMethods);
+
+// Get disease control methods by disease name
+router.get(
+  "/disease-control/:diseaseName",
+  getDiseaseControlMethodsByDiseaseName
+);
+
+// Create new disease control method
+router.post("/disease-control", createDiseaseControlMethod);
+
+// Update disease control method by name
+router.patch("/disease-control/:diseaseName", updateDiseaseControlMethodByName);
+
+// Delete disease control method by name
+router.delete(
+  "/disease-control/:diseaseName",
+  deleteDiseaseControlMethodByName
+);
 
 module.exports = router;
