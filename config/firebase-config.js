69a2123679db220674fd4633118f368e32d4a51d
danmesfin <danielmsfn@gmail.com>
2023-05-18 09:45:40 +0300

    updated admin import

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://sebl-farm-assist.firebaseio.com",
-});
+console.log("service account", serviceAccount),
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://sebl-farm-assist.firebaseio.com",
+  });
 
 const db = admin.firestore();
 
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-const admin = require("../config/firebase-config");
+const { admin } = require("../config/firebase-config");
 
 const verifyToken = (req, res, next) => {
   const idToken = req.headers.authorization.split(" ")[1];
