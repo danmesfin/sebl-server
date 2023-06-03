1b195f958c5217c587ac78a7e7a283e02a92a739
danmesfin <danielmsfn@gmail.com>
2023-06-03 12:15:42 +0300

    updated users controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,16 +1,54 @@
-const getUser = (req, res) => {
-  const userId = req.params.id;
-  // TODO: Retrieve user from database using the userId
-  const user = { id: userId, name: "John Doe" };
-  res.send(user);
-};
+const { db } = require("../config/firebase-config");
 
-const getUsers = (req, res) => {
-  // TODO: Retrieve all users from the database
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Doe" },
-  ];
-  res.send(users);
-};
-module.exports = { getUser, getUsers };
+// Create a new user
+async function createUser(req, res) {
+  try {
+    const { name, email } = req.body;
+    const { uid } = req.user; // Access the decoded user ID from the middleware
+
+    // Add the user to the Firestore collection
+    const newUserRef = await db.collection("users").add({
+      name,
+      email,
+      uid, // Store the decoded user ID in the Firestore document
+    });
+
+    res.status(201).json({ id: newUserRef.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+// Get all users
+async function getAllUsers(req, res) {
+  try {
+    const usersRef = db.collection("users");
+    const snapshot = await usersRef.get();
+    const users = [];
+    snapshot.forEach((doc) => {
+      users.push({ id: doc.id, ...doc.data() });
+    });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+// Get a user by ID
+async function getUserById(req, res) {
+  try {
+    const userRef = db.collection("users").doc(req.params.id);
+    const user = await userRef.get();
+    if (!user.exists) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ id: user.id, ...user.data() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+module.exports = { createUser, getAllUsers, getUserById };
